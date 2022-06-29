7a5bd218e3804ba7d753712f86d9407d0edd5a24
masoud <otienoamba81@gmail.com>
2022-06-30 01:01:59 +0300

    Removed modalsheet style

--- welcome.php
+++ welcome.php
@@ -371,8 +371,9 @@ span.psw {
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
-                modal.style.display = "none";
+                // modal.style.display = "none"; 
             }
+           
         }
         </script>
 </body>
