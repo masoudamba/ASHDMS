d443123d4bd0d156165cb2a75feaa7501770acc7
BEN7MAGARE <magaben33@gmail.com>
2022-09-10 11:12:31 +0300

    Mpesa done

--- callback.php
+++ callback.php
@@ -1,21 +1,10 @@
 <?php
-
-include("config.php");
-include("function.php");
-
+require 'backend/db.php';
 date_default_timezone_set("Africa/Nairobi");
 header("Access-Control-Allow-Origin: *");
 $data=file_get_contents('php://input');
-
-
-
-$user_data = "details=";
-try {
-    session_start();
-    $user_data = "details=". $_SESSION['ParentID'];
-} catch (\Throwable $th) {
-//throw $th;                
-}
+$dba = new DBA();
+$db = $dba->db;
 
 if(!empty($data)){
     $json=json_decode($data);
@@ -25,11 +14,8 @@ if(!empty($data)){
     $checkOutId=$stkCallback->CheckoutRequestID;
     $ResultCode=$stkCallback->ResultCode;
     if($ResultCode!=0){
-        session('error','Transaction not successful! Please retry');
-
-        header("Location: parent.php?error=Payment Failed&$user_data");
+        header("Location: parent.php?error=Payment Failed & Error occured during processing");
         exit();
-
     }else{
         $CallbackMetadata=$stkCallback->CallbackMetadata;
         $Item=$CallbackMetadata->Item;
@@ -42,52 +28,14 @@ if(!empty($data)){
         $Amount=$Amount->Value;
         if($MpesaReceiptNumber!==null&&$MpesaReceiptNumber!=="")
         {
-            // You are suppose to write sql that queries the database to get a transaction on payments tablw where query_id=$checkOutId set receipt number to  MpesaReceiptNumber and status to paid.
-            //$checkOutId,$MpesaReceiptNumber
             try {
-                
-                $sql_cases = "SELECT * FROM payment WHERE query_id = $checkOutId";
-                $qry1 = mysqli_query($con, $sql_cases);
-                
-
-                if((mysqli_num_rows($qry1)) > 0){
-                    $query2 = "UPDATE payment 
-                        SET phone_number = '$PhoneNumber', mpesa_receipt = '$MpesaReceiptNumber', status='PAID', 
-                        amount='$Amount' WHERE query_id = '$checkOutId'";
-
-                    $result2 = mysqli_query($con, $query2);
-
-                    if ($result2) {
-                        header("Location: parent.php?error=Payment successfully processed&$user_data");
-                        exit();
-                    }else {
-                        header("Location: parent.php?error=Payment Failed&$user_data");
-                        exit();
-                    }
-
-                }
-
-
-
+                $stmt = $db->prepare("UPDATE payment SET `mpesa_receipt`=:MpesaReceiptNumber, `status`=:status WHERE `query_id`=:checkOutId");
+                $stmt->execute([':receipt'=>$MpesaReceiptNumber, ':status'=>'paid', ':checkOutId'=> $checkOutId]);
+                header("Location: parent.php?error=Payment successfully processed");
             } catch (\Throwable $th) {
-                //throw $th;
-                header("Location: parent.php?error=Payment Failed&$user_data");
+                header("Location: parent.php?error=Payment Failed");
                 exit();
             }
-            // redirect to preffered page the error??
-            //session('success','Transaction was successful');
-            // the table structure for payments is suppose to have the followng structure
-            // id
-            // user_id
-            // phone_number
-            // mpesa_receipt
-            // query_id
-            // amount
-            // status
-
-            // i hope that will be of help to you
-            // You are suppose to change the call back url on payment.php to point to this file and precede it with required url. You are also suppose to change the action on form to point to payment.php. All the best
-
         }
     }
 }else{
--- payment.php
+++ payment.php
@@ -1,103 +1,82 @@
 <?php
-
-include("config.php");
-include("function.php");
-
 session_start();
+require 'backend/db.php';
+$dba = new DBA();
 
-$user_data = "details=";
-try {
-    //session_start();
-    $user_data = "details=". $_SESSION['ParentID'];
-} catch (\Throwable $th) {
-//throw $th;                
-}
-
-$errorDB = "Payment Request Sent";
+$url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$access_token = '';
 
 if (isset($_POST['amount'])) {
-    $amount = htmlspecialchars(strip_tags($_POST["amount"]));
-    $phone = htmlspecialchars(strip_tags($_POST["phone"]));
-
-    $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-    $access_token = '';
-    $consumer_secret = 'hOAX5KXA3OSUmoqc';
-    $consumer_key = '9qpMAObAcG4WRDWDPb5ykegBXpNlTAOq';
-    $encodestring = base64_encode($consumer_key.":".$consumer_secret);
+    $consumer_secret = 'KEPgfS1AbNtQeRaL';
+    $consumer_key = '9u589pJDEzppBPkYbKeYvvrtGGYPtb5F';
+    $encodestring = base64_encode($consumer_key . ":" . $consumer_secret);
+    $OuathString = 'Basic ' . $encodestring;
 
     $oauthURL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $oauthURL);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$encodestring));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $encodestring)); //setting a custom header
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
     $curl_response = curl_exec($curl);
     $json = json_decode($curl_response, true);
 
     $access_token = $json['access_token'];
     $passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
-    $timestamp = '30'.date("ymdhis");
-    $password = base64_encode('174379'.$passkey.$timestamp);
+    $timestamp = '30' . date("ymdhis");
+    $password = base64_encode('174379' . $passkey . $timestamp);
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$access_token));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $access_token)); //setting custom header
+
+    $amount = (int)htmlspecialchars(strip_tags($_POST["amount"]));
+    $phone = htmlspecialchars(strip_tags($_POST["phone"]));
+    $name = $_SESSION["name"];
+    $id = $_SESSION['id'];
 
     $curl_post_data = array(
-    'BusinessShortCode' => '174379',
-    'Password' => $password,
-    'Timestamp' => $timestamp,
-    'TransactionType' => 'CustomerPayBillOnline',
-    'Amount' => $amount,
-    'PartyA' => $phone,
-    'PartyB' => '174379',
-    'PhoneNumber' => $phone,
-    'CallBackURL' => 'https://1b7c-2c0f-fe38-2247-68bc-802-baa1-4268-39d1.in.ngrok.io/ASHDMS/callback.php',
-    'AccountReference' => $_SESSION["ParentID"].' '.$timestamp ,
-    'TransactionDesc' => 'Paybill online - '.date("F")
+        'BusinessShortCode' => '174379',
+        'Password' => $password,
+        'Timestamp' => $timestamp,
+        'TransactionType' => 'CustomerPayBillOnline',
+        'Amount' => $amount,
+        'PartyA' => $phone,
+        'PartyB' => '174379',
+        'PhoneNumber' => $phone,
+        'CallBackURL' => 'https://f12c-102-135-169-115.in.ngrok.io/ASHDMS/callback.php',
+        'AccountReference' => 'Parent'.$id . $timestamp,
+        'TransactionDesc' => 'Parent payment - ' . date("F")
     );
 
     $data_string = json_encode($curl_post_data);
+
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
     $curl_response = curl_exec($curl);
-    $data_string= json_decode($curl_response, true);
-
-    if(isset($data_string["ResponseCode"]) && $data_string["ResponseCode"] == 0){
-        $_SESSION['CheckoutID'] = $data_string['CheckoutRequestID'];
-        $checkoutId = $data_string['CheckoutRequestID'];
-        // You are suppose to write a sql statement to save below data to the database
-        $parentID = $_SESSION["ParentID"];
-        $sessionId = 0;
-        $con= mysqli_connect("localhost","root","","ashdms");
-        $sql_cases = "INSERT INTO payment(parent_id, user_id, phone_number, query_id, amount) 
-        VALUES ('$parentID', $sessionId, '$phone', '$checkoutId', '$amount')";
-        try {
-            $qry1 = mysqli_query($con, $sql_cases);
-        } catch (\Throwable $th) {
-            throw $th;
-        }
-        die('kdjjjd');
-        return true;
+    
+    $data = json_decode($curl_response, true);
+    $_SESSION['CheckoutID'] = json_decode($curl_response, true)['CheckoutRequestID'];
+    
+    $db = $dba->db;
+    if ($data['ResponseCode'] == 0) {
+        $checkoutId = $data['CheckoutRequestID'];
+        $parent_id = $_SESSION['id'];
         
-    }elseif ($data_string["errorCode"] == "400.002.02") {
-       $_SESSION['error'] = $data_string["errorMessage"];
-       $errorDB = "Payment Request Failed";
-        return false;
-    }else {
-        $errorDB = "Payment Request Failed";
-        return false;
+        try {
+            $stmt = $db->prepare("INSERT INTO payment(`parent_id`,`phone_number`, `query_id`, `amount`)
+                VALUES (:parent_id, :phonenumber, :checkoutId, :amount)");
+            $stmt->execute([':parent_id'=>$parent_id, ':phonenumber'=>$phone, ':checkoutId'=>$checkoutId, ':amount'=>$amount]);
+            header("Location: parent.php?success=Payment accepted for processing. Check your phone and complete the transaction");
+        } catch (PDOException $e) {
+            $error = $e->getMessage();
+            header("Location: parent.php?error=Error occured during processing. Ensure all details are filled and retry");
+        }
     }
-}else{
-    $errorDB = "Payment Request Failed";
 }
 
-try {
-    header("Location: parent.php?error=$errorDB&$user_data");
-	exit();
-} catch (\Throwable $th) {
-    //throw $th;
-}
 
 ?>
