a7c9d128525f17a308bd93f018fe4b3a3269acf1
masoud <otienoamba81@gmail.com>
2022-09-10 12:42:51 +0300

    some changes

--- payment.php
+++ payment.php
@@ -2,16 +2,13 @@
 session_start();
 require 'backend/db.php';
 $dba = new DBA();
-
 $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 $access_token = '';
-
 if (isset($_POST['amount'])) {
     $consumer_secret = 'KEPgfS1AbNtQeRaL';
     $consumer_key = '9u589pJDEzppBPkYbKeYvvrtGGYPtb5F';
     $encodestring = base64_encode($consumer_key . ":" . $consumer_secret);
     $OuathString = 'Basic ' . $encodestring;
-
     $oauthURL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $oauthURL);
@@ -19,10 +16,8 @@ if (isset($_POST['amount'])) {
     curl_setopt($curl, CURLOPT_HEADER, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
     $curl_response = curl_exec($curl);
     $json = json_decode($curl_response, true);
-
     $access_token = $json['access_token'];
     $passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
     $timestamp = '30' . date("ymdhis");
@@ -30,26 +25,11 @@ if (isset($_POST['amount'])) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer ' . $access_token)); //setting custom header
-
     $amount = (int)htmlspecialchars(strip_tags($_POST["amount"]));
     $phone = htmlspecialchars(strip_tags($_POST["phone"]));
     $name = $_SESSION["name"];
     $id = $_SESSION['id'];
-
     $curl_post_data = array(
-<<<<<<< HEAD
-    'BusinessShortCode' => '174379',
-    'Password' => $password,
-    'Timestamp' => $timestamp,
-    'TransactionType' => 'CustomerPayBillOnline',
-    'Amount' => $amount,
-    'PartyA' => $phone,
-    'PartyB' => '174379',
-    'PhoneNumber' => $phone,
-    'CallBackURL' => 'https://ce98-105-163-0-205.ap.ngrok.io/ASHDMS/callback.php',
-    'AccountReference' => $_SESSION["ParentID"].' '.$timestamp ,
-    'TransactionDesc' => 'Paybill online - '.date("F")
-=======
         'BusinessShortCode' => '174379',
         'Password' => $password,
         'Timestamp' => $timestamp,
@@ -61,35 +41,12 @@ if (isset($_POST['amount'])) {
         'CallBackURL' => 'https://f12c-102-135-169-115.in.ngrok.io/ASHDMS/callback.php',
         'AccountReference' => 'Parent'.$id . $timestamp,
         'TransactionDesc' => 'Parent payment - ' . date("F")
->>>>>>> 9529dfc5bcdf820fadb21e6fdf6b0c0cdc94633b
     );
-
     $data_string = json_encode($curl_post_data);
-
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-
     $curl_response = curl_exec($curl);
-<<<<<<< HEAD
-    $data_string= json_decode($curl_response, true);
-
-    if(isset($data_string["ResponseCode"]) && $data_string["ResponseCode"] == 0){
-        $_SESSION['CheckoutID'] = $data_string['CheckoutRequestID'];
-        $checkoutId = $data_string['CheckoutRequestID'];
-        // You are suppose to write a sql statement to save below data to the database
-        $parentID = $_SESSION["ParentID"];
-        $sessionId = 0;
-        $sql_cases = "INSERT INTO payment (parent_id, user_id, phone_number, query_id,
-            amount) VALUES ($parentID, $sessionId, $phone, $checkoutId, $amount)";
-        try {
-            $qry1 = mysqli_query($con, $sql_cases);
-        } catch (\Throwable $th) {
-            throw $th;
-        }
-        die('kdjjjd');
-        return true;
-=======
     
     $data = json_decode($curl_response, true);
     $_SESSION['CheckoutID'] = json_decode($curl_response, true)['CheckoutRequestID'];
@@ -98,7 +55,6 @@ if (isset($_POST['amount'])) {
     if ($data['ResponseCode'] == 0) {
         $checkoutId = $data['CheckoutRequestID'];
         $parent_id = $_SESSION['id'];
->>>>>>> 9529dfc5bcdf820fadb21e6fdf6b0c0cdc94633b
         
         try {
             $stmt = $db->prepare("INSERT INTO payment(`parent_id`,`phone_number`, `query_id`, `amount`)
@@ -111,5 +67,6 @@ if (isset($_POST['amount'])) {
         }
     }
 }
+
 
 ?>
